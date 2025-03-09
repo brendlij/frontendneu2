687878c583eb1670550b563b6b5ae3399983b225
brendlij <brendlij@dhbw-loerrach.de>
2025-03-09 16:32:29 +0100

    Geht 90%

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
-
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <footer className="footer">
+      <div className="container">
+        <span className="text-muted">
+          <ul>
+            <li>
+              <a href="Kontakt">Kontakt</a>
+            </li>
+          </ul>
+        </span>
+      </div>
     </footer>
   );
 }
--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
-import Carousel from 'react-bootstrap/Carousel';
-import Slidepic1 from '../assets/Slidepic1.jpg';
-import Slidepic2 from '../assets/Slidepic2.jpg';
-import Slidepic3 from '../assets/Slidepic3.jpg';
+import Carousel from "react-bootstrap/Carousel";
+import Slidepic1 from "../assets/Slidepic1.jpg";
+import Slidepic2 from "../assets/Slidepic2.jpg";
+import Slidepic3 from "../assets/Slidepic3.jpg";
+import Slidepic4 from "../assets/Slidepic4.jpg";
+import Slidepic5 from "../assets/Slidepic5.jpg";
 
 const data = [
   {
     image: Slidepic1,
-    caption: "Caption",
-    description: "Description Here"
+    caption: "Freude und Spaß am Lernen",
+    description: "Entdecke die Welt der Mathematik und Informatik",
   },
   {
     image: Slidepic2,
-    caption: "Caption",
-    description: "Description Here"
+    caption: "Zusammenarbeit und Teamwork",
+    description: "Gemeinsam lernen und wachsen",
   },
   {
     image: Slidepic3,
-    caption: "Caption",
-    description: "Description Here"
-  }
-]
+    caption: "Chancengleichheit",
+    description: "Platz für Alle. Wir fördern Vielfalt",
+  },
+  {
+    image: Slidepic4,
+    caption: "Ambiente und Atmosphäre",
+    description: "Lernen in einer angenehmen Umgebung",
+  },
+  {
+    image: Slidepic5,
+    caption: "Organisiert und Strukturiert",
+    description: "Wir helfen dir, deinen Weg zu finden",
+  },
+];
 
 function HomeCarousel() {
   const [index, setIndex] = useState(0);
@@ -28,26 +40,25 @@ function HomeCarousel() {
     setIndex(selectedIndex);
   };
 
-
   return (
-
-    <div style={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh',
-      overflow: 'hidden'
-    }}>
-
-      <Carousel activeIndex={index} onSelect={handleSelect} interval={3000} >
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <Carousel activeIndex={index} onSelect={handleSelect} interval={5000}>
         {data.map((slide, i) => (
-          <Carousel.Item key={i} style={{ height: '100vh' }}>
+          <Carousel.Item key={i} style={{ height: "100vh" }}>
             <img
               className="d-block w-100"
               src={slide.image}
               alt="slider image"
-              style={{ objectFit: 'cover', width: '100%', height: '100vh' }}
+              style={{ objectFit: "cover", width: "100%", height: "100vh" }}
             />
             <Carousel.Caption>
               <h3>{slide.caption}</h3>
@@ -57,10 +68,7 @@ function HomeCarousel() {
         ))}
       </Carousel>
     </div>
-
-
   );
 }
 
 export default HomeCarousel;
-
